299b625d016588b093544d6cbdb5adb39e137b80
unknown <fnicolegm12@outlook.com>
2026-05-10 11:05:17 -0600

    implement semester filtering

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from utils.excel_manager import cargar_y_normalizar
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
 
-    registros = cargar_y_normalizar("software")
+    carrera = "software"
+    semestre_seleccionado = ""
+
+    if request.method == "POST":
+
+        carrera = request.form.get("carrera")
+
+        semestre_seleccionado = request.form.get("semestre")
+
+    registros = cargar_y_normalizar(carrera)
+
+    # Obtener semestres únicos
+    semestres = []
+
+    for r in registros:
+
+        semestre = r["semestre"]
+
+        if semestre not in semestres:
+
+            semestres.append(semestre)
+
+    # Filtrar por semestre
+    if semestre_seleccionado != "":
+
+        registros = [
+
+            r for r in registros
+
+            if r["semestre"] == semestre_seleccionado
+        ]
 
     return render_template(
         "index.html",
-        registros=registros
+        registros=registros,
+        semestres=semestres,
+        semestre_seleccionado=semestre_seleccionado
     )
 
 if __name__ == "__main__":
